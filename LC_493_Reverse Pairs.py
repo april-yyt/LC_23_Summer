62f2285cf516694fb84860d32274e7161cdf461f
april-yyt <aprilytyang@gmail.com>
2023-08-03 22:44:51 -0700

    0803-1

---
+++ LC_493_Reverse Pairs.py
@@ -0,0 +1,27 @@
+## Array
+## Sorting
+## Merge Sort
+
+### Date: 08/03/2023
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        cnt = 0
+
+        def merge(left, right):
+            nonlocal cnt
+            i = j = 0
+            while i < len(left) and j < len(right):
+                if left[i] <= 2*right[j]:
+                    i += 1
+                else:
+                    cnt += len(left) - i
+                    j += 1
+            return sorted(left+right)
+        
+        def mergeSort(A):
+            if len(A) <= 1:
+                return A
+            return merge(mergeSort(A[:(len(A)+1)//2]), mergeSort(A[(len(A)+1)//2:]))
+        
+        mergeSort(nums)
+        return cnt
